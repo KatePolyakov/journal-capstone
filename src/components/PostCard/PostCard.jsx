8abee559571dbc457c7c2f21a7b05e84a95947ad
Kate <polyakovkate@gmail.com>
2024-04-30 19:45:47 -0700

    connected home page with backend

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 
-import ProfileImg from '../../assets/download.jpg';
-import MainPhoto from '../../assets/restaurantPhoto.jpeg';
 import './PostCard.scss';
 
-export const PostCard = () => {
+export const PostCard = ({ getAllposts }) => {
   return (
-    <div className="post-card">
-      <div className="post-card__wrapper">
-        <div className="post-card__header">
-          <img src={ProfileImg} alt="Profile" className="post-card__profile-img" />
-          <div className="post-card__name-group">
-            <p className='post-card__name'>Blogs_Name</p>
-            <p className='post-card__date'>29 april 2024</p>
+    <>
+      {getAllposts?.map((post) => (
+        <div className="post-card" key={post._id}>
+          <div className="post-card__wrapper">
+            <div className="post-card__header">
+              <img src={post.user.avatarUrl} alt="Profile" className="post-card__profile-img" />
+              <div className="post-card__name-group">
+                <p className="post-card__name">{post.user.fullName}</p>
+                <p className="post-card__date">{post.createdAt}</p>
+              </div>
+            </div>
+            <div className="post-card__main">
+              <img src={post.imageUrl} alt="main" className="post-card__main-img" />
+              <p className="post-card__title">{post.title}</p>
+            </div>
           </div>
         </div>
-        <div className='post-card__main'>
-          <img src={MainPhoto} alt='main' className='post-card__main-img'/>
-          <p className='post-card__title'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-        </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { PostCard } from '../../components/PostCard/PostCard';
 
 import './HomePage.scss';
+import axios from 'axios';
 
 export const HomePage = () => {
+  const [getAllposts, setGetAllPosts] = useState();
+
+  useEffect(() => {
+    const fetchAllPosts = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/posts`);
+        //console.log(response.data);
+        setGetAllPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching all posts:', error);
+      }
+    };
+    fetchAllPosts();
+  }, []);
+
+  console.log(getAllposts);
+
   return (
-    <div className="homePage">
-      <div className="homePage__wrapper">
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
+    <div className="home-page">
+      <div className="home-page__wrapper">
+        <PostCard getAllposts={getAllposts} />
       </div>
     </div>
   );
