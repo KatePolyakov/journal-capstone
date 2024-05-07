628fea745d6d6ef289262ee0aa5ce66499a3b86e
Kate <polyakovkate@gmail.com>
2024-05-06 22:01:25 -0700

    added hardcoded tags

---
+++ src/components/Tags/Tags.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+import './Tags.scss';
+
+
+
+export const Tags = () => {
+
+
+  return (
+    <div className="tags">
+      <div className="tags__wrapper">
+        <div className="tags__text">
+        <p># museum</p>
+        <p># train</p>
+        <p># food</p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,7 @@ import { fetchPosts } from '../../redux/slices/posts';
 import { Post } from '../../components/Post/Post';
 
 import './HomePage.scss';
+import { Tags } from '../../components/Tags/Tags';
 
 export const HomePage = () => {
   const dispatch = useDispatch();
@@ -16,28 +17,32 @@ export const HomePage = () => {
   useEffect(() => {
     dispatch(fetchPosts());
   }, []);
+
   return (
     <>
       <div className="home-page">
         <div className="home-page__wrapper">
-          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
-            isPostsLoading ? (
-              <Post key={index} isLoading={true} />
-            ) : (
-              <Post
-                key={obj._id}
-                id={obj._id}
-                title={obj.title}
-                imageUrl={obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ''}
-                user={obj.user}
-                createdAt={obj.createdAt}
-                viewsCount={obj.viewsCount}
-                commentsCount={3}
-                tags={obj.tags}
-                isEditable={userData?._id === obj.user._id}
-              />
-            ),
-          )}
+          <Tags />
+          <div className="home-page__posts">
+            {(posts?.items).map((obj, index) =>
+              isPostsLoading ? (
+                <Post key={index} isLoading={true} />
+              ) : (
+                <Post
+                  key={obj._id}
+                  id={obj._id}
+                  title={obj.title}
+                  imageUrl={obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ''}
+                  user={obj.user}
+                  createdAt={obj.createdAt}
+                  viewsCount={obj.viewsCount}
+                  commentsCount={3}
+                  tags={obj.tags}
+                  isEditable={userData?._id === obj.user._id}
+                />
+              ),
+            )}
+          </div>
         </div>
       </div>
     </>
